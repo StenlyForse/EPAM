f2246c154c2b0f961ca23d419d8d1e0fd267345c
Kurlin Alexandr <sasha-kurlin@mail.ru>
2022-02-16 13:25:16 +0300

    Strings Overview. Formatting, Parsing, Comparing

---
+++ M03/M03/AvgWordLen.cs
@@ -0,0 +1,27 @@
+﻿namespace M03
+{
+    using System.Text;
+
+    internal class AvgWordLen
+    {
+        public static double AverageLength(string str)
+        {
+            StringBuilder sb = new StringBuilder();
+            Console.WriteLine("Inputed string: " + str);
+            foreach (char c in str)
+            {
+                if (char.IsLetter(c) || char.IsWhiteSpace(c))
+                {
+                    sb.Append(c);
+                }
+            }
+
+            str = sb.ToString();
+            double spacesCount = str.Count(char.IsWhiteSpace);
+            double wordCount = str.Split(' ', StringSplitOptions.RemoveEmptyEntries).Length;
+            if (wordCount == 0)
+                throw new ArgumentException("String does not contain letters");
+            return (str.Length - spacesCount) / wordCount;
+        }
+    }
+}
---
+++ M03/M03/ReverseString.cs
@@ -0,0 +1,12 @@
+﻿namespace M03
+{
+    internal class ReverseString
+    {
+        public static string ReverseWords(string origString)
+        {
+            string[] temp = origString.Split(' ', StringSplitOptions.None);
+            Array.Reverse(temp);
+            return string.Join(" ", temp);
+        }
+    }
+}
