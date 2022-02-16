5957965949c845d4a72a983590d106c112326016
Kurlin Alexandr <sasha-kurlin@mail.ru>
2022-02-16 13:33:59 +0300

    Introduction to the C# Language, Basic Coding in C#

---
+++ M01/M01/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using ArrayHelper;
+using RectangleHelper;
+
+namespace M01
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            int[] arr = new int[5] { 5, 1, 3, 2, 4 };
+            BubbleSort bubbleSort = new BubbleSort(arr, BubbleSort.SortOrder.Ascending);
+            Console.WriteLine("Unsorted array:");
+            for (int i = 0; i < arr.Length; i++)
+                Console.Write(arr[i] + " ");
+
+            bubbleSort.Sort();
+
+            Console.WriteLine('\n');
+            Console.WriteLine("Sorted array:");
+            for (int i = 0; i < arr.Length; i++)
+                Console.Write(arr[i] + " ");
+
+            Console.WriteLine('\n');
+            int[,] numbers =
+            {
+                { 1, 2, -3 },
+                { 4, -5, 6 }
+            };
+            int rows = numbers.GetLength(0);
+            int columns = numbers.GetLength(1);
+
+            Console.WriteLine("2D array:");
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"{numbers[i, j]} \t");
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+
+            TwoDimArrSumBase abc = new TwoDimArrSumBase();
+            Console.WriteLine("Sum of the all positive elements:");
+            Console.WriteLine(abc.Sum(numbers));
+            Console.Write("Enter a width of a rectangle >>");
+            int wid = int.Parse(Console.ReadLine());
+            Console.Write("Enter a hight of a rectangle >>");
+            int hei = int.Parse(Console.ReadLine());
+            Rectangle rec = new Rectangle(wid, hei);
+
+            var results = new List<ValidationResult>();
+            var context = new ValidationContext(rec);
+            if (!Validator.TryValidateObject(rec, context, results, true))
+            {
+                foreach (var error in results)
+                {
+                    Console.WriteLine(error.ErrorMessage);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Perimetr of a rectangle:");
+                Console.WriteLine(rec.Perimetr());
+                Console.WriteLine("Square of a rectangle");
+                Console.WriteLine(rec.Square());
+            }
+        }
+    }
+}
---
+++ M01/RectangleHelper/RectangleHelper.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RectangleHelper
+{
+    public class Rectangle
+    {
+        [Required]
+        [Range(0, int.MaxValue, ErrorMessage = "Enter value greater than 0")]
+        public int Width { get; private set; }
+
+        [Required]
+        [Range(1, int.MaxValue, ErrorMessage = "Enter value greater than 0")]
+        public int Height { get; private set; }
+
+        public Rectangle(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public int Perimetr()
+        {
+            return 2 * (Width + Height);
+        }
+
+        public int Square()
+        {
+            return Width * Height;
+        }
+    }
+}
